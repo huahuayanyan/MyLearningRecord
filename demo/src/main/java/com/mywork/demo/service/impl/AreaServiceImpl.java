05f6e586bae17c00552641f69bb5661d6daf4e14
wangyan <1545109330@qq.com>
2018-08-23 06:01:00 +0800

    service实现

---
+++ demo/src/main/java/com/mywork/demo/service/impl/AreaServiceImpl.java
@@ -0,0 +1,92 @@
+package com.mywork.demo.service.impl;
+
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.mywork.demo.dao.AreaDao;
+import com.mywork.demo.entity.Area;
+import com.mywork.demo.service.AreaService;
+
+@Service
+public class AreaServiceImpl implements AreaService{
+
+	private AreaDao areaDao;
+	
+	@Override
+	public List<Area> queryArea() {
+		return areaDao.queryArea();
+	}
+
+	@Override
+	public Area queryAreaById(int areaId) {
+		return areaDao.queryAreaById(areaId);
+	}
+
+	@Transactional
+	@Override
+	public boolean insertArea(Area area) {
+		if(area.getAreaName() != null && !"".equals(area.getAreaName())) {
+			area.setCreateTime(new Date());
+			area.setLastEditTime(new Date());
+			try {
+				int effectedNum = areaDao.insertArea(area);
+				if(effectedNum > 0) {
+					return true;
+				} else {
+					throw new RuntimeException("插入区域信息失败!");
+				}
+				
+			} catch (Exception e) {
+				throw new RuntimeException("插入区域信息失败:" + e.getMessage());
+			}
+			
+		} else {
+			throw new RuntimeException("区域信息不能为空!");
+		}
+	}
+
+	@Override
+	public boolean updateArea(Area area) {
+		if(area.getAreaId() != null && !"".equals(area.getAreaId())) {
+			area.setLastEditTime(new Date());
+			try {
+				int effectedNum = areaDao.updateArea(area);
+				if(effectedNum > 0) {
+					return true;
+				} else {
+					throw new RuntimeException("插入区域信息失败!");
+				}
+				
+			} catch (Exception e) {
+				throw new RuntimeException("插入区域信息失败:" + e.getMessage());
+			}
+			
+		} else {
+			throw new RuntimeException("区域信息不能为空!");
+		}
+	}
+
+	@Override
+	public boolean deleteArea(int areaId) {
+		if(areaId > 0) {
+			try {
+				int effectedNum = areaDao.deleteArea(areaId);
+				if(effectedNum > 0) {
+					return true;
+				} else {
+					throw new RuntimeException("插入区域信息失败!");
+				}
+				
+			} catch (Exception e) {
+				throw new RuntimeException("插入区域信息失败:" + e.getMessage());
+			}
+			
+		} else {
+			throw new RuntimeException("区域信息不能为空!");
+		}
+	}
+
+}
